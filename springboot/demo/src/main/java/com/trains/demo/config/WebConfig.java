29c10ec06c83cf057489a0460eab8bc6b1f4db56
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-06-26 23:44:14 +0200

    Cors update

--- springboot/demo/src/main/java/com/trains/demo/config/WebConfig.java
+++ springboot/demo/src/main/java/com/trains/demo/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://trains-backend-bvln.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
