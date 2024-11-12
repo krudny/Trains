0244929abba4db7abd35c0b2a55f919577c31730
Kamil Rudny <k.rudny1@gmail.com>
2024-11-13 00:05:27 +0100

    Basic hero design

--- front-refactor/app/layout.tsx
+++ front-refactor/app/layout.tsx
@@ -4,8 +4,14 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+          <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+      </head>
       <body className={"antialiased"}>{children}</body>
-    </html>
+      </html>
   );
 }
--- front-refactor/tailwind.config.ts
+++ front-refactor/tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Aeonik'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
